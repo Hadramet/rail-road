93940d72a3a35fe6af8873660e1a49238eaa3596
hadramet <hadramet.sylla@outlook.fr>
2022-12-01 17:05:06 +0100

    feat : add card registry to smart contract

--- smart-contracts/contracts/Railroad.sol
+++ smart-contracts/contracts/Railroad.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "./RailroadBase.sol";
+import "./RailroadCard.sol";
 import {RailRoadRessources as Res} from "./RailroadLib.sol";
 
 /// @title Railroad
 /// @author Railroad is the entry point of the contract
 /// @notice It control the operation of setting new withdrawal address.
-contract Railroad is RailroadBase {
+contract Railroad is RailroadCard {
     constructor() {
         withdrawalAddress = msg.sender;
     }
--- smart-contracts/test/Railroad.js
+++ smart-contracts/test/Railroad.js
@@ -2,33 +2,77 @@ const { expect } = require("chai");
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("Railroad contract", () => {
+  const deployFixture = async () => {
+    const Railroad = await ethers.getContractFactory("Railroad");
+    const [owner, addr1, addr2] = await ethers.getSigners();
 
-    const deployFixture = async () => {
-        const Railroad = await ethers.getContractFactory("Railroad");
-        const [owner, addr1, addr2] = await ethers.getSigners();
-        
-        const hardhatRailroad = await Railroad.deploy();
-        await hardhatRailroad.deployed();
-        return { Railroad, hardhatRailroad, owner, addr1, addr2 };
-    }
+    const hardhatRailroad = await Railroad.deploy();
+    await hardhatRailroad.deployed();
+    return { Railroad, hardhatRailroad, owner, addr1, addr2 };
+  };
 
+  describe("Deployment", () => {
+    it("Should set the right owner", async () => {
+      const { hardhatRailroad, owner } = await loadFixture(deployFixture);
+      expect(await hardhatRailroad.owner()).to.eq(owner.address);
+    });
 
-    describe("Deployment", () => {
+    it("Should set the withdrawal address to owner by default", async () => {
+      const { hardhatRailroad, owner } = await loadFixture(deployFixture);
+      expect(await hardhatRailroad.withdrawalAddress()).to.eq(owner.address);
+    });
 
-        it("Should set the right owner", async () => {
-            const { hardhatRailroad, owner } = await loadFixture(deployFixture);
-            expect(await hardhatRailroad.owner()).to.eq(owner.address);
-        })
+    it("Setting withdrawal address should work", async () => {
+      const { hardhatRailroad, addr1 } = await loadFixture(deployFixture);
+      await hardhatRailroad.setWithdrawalAddress(addr1.address);
+      expect(await hardhatRailroad.withdrawalAddress()).to.eq(addr1.address);
+    });
+  });
 
-        it("Should set the withdrawal address to owner by default", async () => {
-            const { hardhatRailroad, owner } = await loadFixture(deployFixture);
-            expect(await hardhatRailroad.withdrawalAddress()).to.eq(owner.address);
-        })
+  describe("Card", () => {
+    it("Adding card emit new card event", async () => {
+      const { hardhatRailroad } = await loadFixture(deployFixture);
 
-        it("Setting withdrawal address should work", async () => {
-            const { hardhatRailroad, addr1 } = await loadFixture(deployFixture);
-            await hardhatRailroad.setWithdrawalAddress(addr1.address);
-            expect(await hardhatRailroad.withdrawalAddress()).to.eq(addr1.address);
-        })
-    })
-})
+      await expect(hardhatRailroad.addCard(1111, 500, 1050, 5000))
+        .to.emit(hardhatRailroad, "NewCard")
+        .withArgs(1111, 500, 1050, 5000);
+    });
+
+    it("Adding card with existing id should faild", async () => {
+      const { hardhatRailroad } = await loadFixture(deployFixture);
+      await hardhatRailroad.addCard(1111, 500, 1050, 5000);
+
+      await expect(
+        hardhatRailroad.addCard(1111, 500, 1050, 5000)
+      ).to.be.revertedWith("Card id already exist");
+    });
+
+    it("Adding card with invalid args should failed", async () => {
+      const { hardhatRailroad } = await loadFixture(deployFixture);
+
+      await expect(
+        hardhatRailroad.addCard(1111, 500, 1050, 0)
+      ).to.be.revertedWith("Invalid total sellable");
+
+      await expect(
+        hardhatRailroad.addCard(1111, 500, 0, 500)
+      ).to.be.revertedWith("Invalid discount.");
+    });
+
+    it("Adding new card should failed on non owner", async () => {
+      const { hardhatRailroad, addr1 } = await loadFixture(deployFixture);
+      await expect(
+        hardhatRailroad.connect(addr1).addCard(1111, 500, 0, 500)
+      ).to.be.revertedWith("Ownable: caller is not the owner");
+    });
+
+    it("Non owner can get list of card ids", async () => {
+      const { hardhatRailroad, addr1 } = await loadFixture(deployFixture);
+      await hardhatRailroad.addCard(1111, 500, 1050, 5000);
+      await hardhatRailroad.addCard(1112, 500, 1050, 5000);
+      const result = await hardhatRailroad.connect(addr1).getAllCardIds();
+      expect(result.length).to.equal(2);
+    });
+    
+  });
+});
