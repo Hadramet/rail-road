720eb794acad0a5d81a2d2937e42e67b24993d49
hadramet <hadramet.sylla@outlook.fr>
2022-12-01 15:23:17 +0100

    add wallet connection using wagmi and connectkit

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,13 +1,21 @@
-import * as React from 'react'
-import type { AppProps } from 'next/app'
-import createEmotionCache from '../src/createEmotionCache'
-import { EmotionCache } from '@emotion/cache'
-import theme from '../src/theme'
-import { CacheProvider, ThemeProvider } from '@emotion/react'
-import Head from 'next/head'
-import { CssBaseline } from '@mui/material'
+import type { AppProps } from 'next/app';
+import createEmotionCache from '../src/createEmotionCache';
+import { EmotionCache } from '@emotion/cache';
+import theme from '../src/theme';
+import { CacheProvider, ThemeProvider } from '@emotion/react';
+import Head from 'next/head';
+import { CssBaseline } from '@mui/material';
+import { createClient,  WagmiConfig, chain  } from 'wagmi';
+import { ConnectKitProvider, ConnectKitButton, getDefaultClient } from "connectkit";
 
 const clientSideEmotionCache = createEmotionCache()
+const chains = [chain.goerli, chain.localhost, chain.hardhat]; //more chain here
+const client = createClient(
+  getDefaultClient({
+    appName: "Your App Name",
+    chains
+  }),
+);
 
 interface RailroadProps extends AppProps {
   emotionCache?: EmotionCache
@@ -17,14 +25,19 @@ interface RailroadProps extends AppProps {
 export default function App(props: RailroadProps) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+    <WagmiConfig client={client}>
+      <ConnectKitProvider>
+        <CacheProvider value={emotionCache}>
+          <Head>
+            <meta name="viewport" content="initial-scale=1, width=device-width" />
+          </Head>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <Component {...pageProps} />
+          </ThemeProvider>
+        </CacheProvider>
+        <ConnectKitButton />
+      </ConnectKitProvider>
+    </WagmiConfig>
   )
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,19 +1,29 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import { Container } from "@mui/system";
+import { useAccount } from "wagmi";
+
+
+function Profile() {
+  const { address, isConnecting, isDisconnected } = useAccount();
+  if (isConnecting) return <div>Connecting...</div>;
+  if (isDisconnected) return <div>Disconnected</div>;
+  return <div>Connected Wallet: {address}</div>;
+}
 
 export default function Home() {
   return (
     <Container maxWidth="lg">
       <Box sx={{
-        my:4,
-        display:'flex',
-        flexDirection:'column',
-        justifyContent:'center',
-        alignItems:'center',
+        my: 4,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Railroad NFT
         </Typography>
+        <Profile />
       </Box>
     </Container>
   )
