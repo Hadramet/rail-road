ef66f93bb76682670060c15940f9cff8bcd7578c
hadramet <hadramet.sylla@outlook.fr>
2022-12-10 15:57:47 +0100

    feat(frontend) : add buying ticket and listing user bought tickes

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -12,30 +12,129 @@ import {
 } from "@mui/material";
 import { Container } from "@mui/system";
 import { AddCardView } from "../components/AddCardView";
-import { useAccount, useContractRead } from "wagmi";
+import { useAccount, useContractRead, useContractWrite, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
 import RailroadArtifact from "../contracts/Railroad.json";
+import RailroadTicketArtifact from "../contracts/RailroadTicket.json";
 import contractAddress from "../contracts/contract-address.json";
 import { useEffect, useState } from "react";
+import { BigNumber } from "ethers";
+import { useDebounce } from "../utils/useDebounce";
+import toast from "react-hot-toast";
+
+const railroadTicketContractConfig = {
+  address: contractAddress.RailroadTicket,
+  abi: RailroadTicketArtifact.abi,
+};
 
 const TransportTickets = () => {
+
+  const [price, setPrice] = useState<number>();
+  const [type, setType] = useState<number>();
+
+  const debouncedPrice = useDebounce(price, 500);
+  const debounceType = useDebounce(type,500);
+
+  const { config } = usePrepareContractWrite({
+    ...railroadTicketContractConfig,
+    functionName: "purchaseTicket",
+    args: [BigNumber.from(debounceType ?? 0)] as const,
+    overrides: {
+      value: BigNumber.from(debouncedPrice ?? 0),
+    },
+  });
+  const { data: buyingData, write } = useContractWrite(config);
+  const { isLoading: isBuyingLoading } = useWaitForTransaction({
+    hash: buyingData?.hash,
+    onSuccess(data) {
+      toast.success(`Success: gaz used ${data.gasUsed}`);
+    },
+    onError(error) {
+      toast.error(`Error ${error.name}`);
+    },
+  });
+
+  const buyTicket = async (type: number) => {
+    let price: number = 0;
+
+    if (type === 1) price = 5000;
+    if (type === 2) price = 6000;
+    if (type === 3) price = 8000;
+
+    setType(type);
+    setPrice(price);
+    
+    write?.();
+  };
+
   return (
-    <Card>
-      <CardMedia
-        component="img"
-        height="100"
-        image="ticket-image.jpg"
-        alt="ticket"
-      />
-      <CardContent>
-        <Typography variant="h6">BUS</Typography>
-        <Typography variant="h6">0.005 ETH</Typography>
-      </CardContent>
-      <CardActions>
-        <Button variant="contained" color="primary">
-          Buy
-        </Button>
-      </CardActions>
-    </Card>
+    <Box>
+      <Card>
+        <CardMedia
+          component="img"
+          height="100"
+          image="ticket-image.jpg"
+          alt="ticket"
+        />
+        <CardContent>
+          <Typography variant="h6">BUS</Typography>
+          <Typography variant="h6">5000 WEI</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="contained"
+            disabled={isBuyingLoading}
+            onClick={() => buyTicket(1)}
+            color="primary"
+          >
+            Buy
+          </Button>
+        </CardActions>
+      </Card>
+      <Card>
+        <CardMedia
+          component="img"
+          height="100"
+          image="ticket-image.jpg"
+          alt="ticket"
+        />
+        <CardContent>
+          <Typography variant="h6">SUBWAY</Typography>
+          <Typography variant="h6">6000 WEI</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="contained"
+            disabled={isBuyingLoading}
+            onClick={() => buyTicket(2)}
+            color="primary"
+          >
+            Buy
+          </Button>
+        </CardActions>
+      </Card>
+      <Card>
+        <CardMedia
+          component="img"
+          height="100"
+          image="ticket-image.jpg"
+          alt="ticket"
+        />
+        <CardContent>
+          <Typography variant="h6">TRAIN</Typography>
+          <Typography variant="h6">5000 WEI</Typography>
+        </CardContent>
+        <CardActions>
+          <Button
+            variant="contained"
+            disabled={isBuyingLoading}
+            onClick={() => buyTicket(3)}
+            color="primary"
+          >
+            Buy
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
   );
 };
 
--- frontend/pages/wallet.tsx
+++ frontend/pages/wallet.tsx
@@ -1,10 +1,12 @@
 import { useAccount, useContractRead } from "wagmi";
 import { readContract, prepareWriteContract, writeContract } from "@wagmi/core";
 import RailroadArtifact from "../contracts/Railroad.json";
+import RailroadTicketArtifact from "../contracts/RailroadTicket.json";
 import contractAddress from "../contracts/contract-address.json";
 import { BigNumber } from "ethers";
 import { useEffect, useState } from "react";
 import {
+  Box,
   Card,
   CardContent,
   Chip,
@@ -14,14 +16,24 @@ import {
   Input,
   InputAdornment,
   InputLabel,
-  TextField,
   Typography,
 } from "@mui/material";
 import { LoadingButton } from "@mui/lab";
+import { formatDistanceToNow } from "date-fns";
 
+const expirationCountDown = (unixDate: number) => {
+  const expirationDate = new Date(unixDate * 1000);
+  let duration = formatDistanceToNow(expirationDate, { addSuffix: true });
+  return duration;
+};
 const railroadContractConfig = {
   address: contractAddress.Railroad,
   abi: RailroadArtifact.abi,
+};
+
+const railroadTicketContractConfig = {
+  address: contractAddress.RailroadTicket,
+  abi: RailroadTicketArtifact.abi,
 };
 
 type userPermit = {
@@ -33,12 +45,28 @@ type userPermit = {
   forsale: boolean;
 };
 
+type UserTicket = {
+  id: number;
+  price: number;
+  expiration: number;
+  type: number;
+};
+
 export default function Wallet() {
   const { address } = useAccount();
   const [permits, setPermits] = useState<userPermit[]>();
+  const [tickets, setTickets] = useState<UserTicket[]>();
   const [updated, setUpdated] = useState<boolean>(false);
+
   const { data } = useContractRead({
     ...railroadContractConfig,
+    functionName: "balanceOf",
+    args: [address],
+    watch: true,
+  });
+
+  const { data: ticketBalance } = useContractRead({
+    ...railroadTicketContractConfig,
     functionName: "balanceOf",
     args: [address],
     watch: true,
@@ -55,6 +83,35 @@ export default function Wallet() {
     setUpdated(false);
   }
 
+  const getTickets = async (balance: number) => {
+    let userTicket: UserTicket[] = [];
+    for (let index = 0; index < balance; index++) {
+      const tokenId = await readContract({
+        ...railroadTicketContractConfig,
+        functionName: "tokenOfOwnerByIndex",
+        args: [address, BigNumber.from(index)] as const,
+      });
+
+      if (!tokenId) continue;
+
+      const ticketInfos = await readContract({
+        ...railroadTicketContractConfig,
+        functionName: "getTicketInfos",
+        args: [BigNumber.from(tokenId)] as const,
+      });
+
+      if (!Array.isArray(ticketInfos)) continue;
+
+      const item: UserTicket = {
+        id: tokenId.toString() as unknown as number,
+        expiration: ticketInfos[1]?.toString() as number,
+        price: ticketInfos[0]?.toString() as number,
+        type: ticketInfos[2]?.toString() as number,
+      };
+      userTicket.push(item);
+    }
+    setTickets(userTicket);
+  };
   async function getPermits(balance: number) {
     let userPermits: userPermit[] = [];
     for (let index = 0; index < balance; index++) {
@@ -107,73 +164,106 @@ export default function Wallet() {
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [address, data, updated]);
+
+  useEffect(() => {
+    if (ticketBalance) {
+      getTickets(parseInt(ticketBalance.toString()));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ticketBalance]);
   return (
     <>
-      {permits && (
-        <Grid container spacing={1}>
-          {permits.map((permit, index) => (
-            <Grid key={index} item>
-              <Card sx={{ display: "flex", m: 2, maxWidth: 300 }}>
-                <CardContent sx={{ flex: "1 0 auto" }}>
-                  <Typography variant="h5">{`Card ID : ${permit.cardId}`}</Typography>
-                  {permit.forsale && <Chip label="For Sale" />}
-                  <Typography variant="subtitle1" color="text.secondary">
-                    {`Buy price : ${permit.price}`}
-                  </Typography>
-                  <Typography variant="caption">
-                    {`Discount : ${permit.discount}`}
-                  </Typography>
-                  <Typography variant="caption">
-                    {`Token ID : ${permit.tokenId}`}
-                  </Typography>
-                  <Typography variant="caption">{`Issued Time : ${permit.issuedTime}`}</Typography>
-                  {!permit.forsale && (
-                    <>
-                      <Divider light sx={{ m: 2 }} />
-                      <FormControl fullWidth sx={{ m: 1 }} variant="standard">
-                        <InputLabel htmlFor="standard-adornment-amount">
-                          Amount
-                        </InputLabel>
-                        <Input
-                          id="standard-adornment-amount"
-                          disabled={permit.forsale}
-                          name={`${permit.tokenId}-input`}
-                          startAdornment={
-                            <InputAdornment position="start">
-                              WEI
-                            </InputAdornment>
-                          }
-                        />
-                        <LoadingButton
-                          sx={{ mt: 1 }}
-                          disabled={permit.forsale}
-                          onClick={() => {
-                            const elements = document.getElementsByName(
-                              `${permit.tokenId}-input`
-                            ) as NodeListOf<HTMLInputElement>;
-
-                            // Check if the elements exist
-                            if (elements.length > 0) {
-                              // Access the first element in the array
-                              const item = elements[0];
-                              const price = parseInt(item.value, 10);
-
-                              setForSale(permit.tokenId, price);
+      <Box>
+        {permits && (
+          <Grid container spacing={1}>
+            {permits.map((permit, index) => (
+              <Grid key={index} item>
+                <Card sx={{ display: "flex", m: 2, maxWidth: 300 }}>
+                  <CardContent sx={{ flex: "1 0 auto" }}>
+                    <Typography variant="h5">{`Card ID : ${permit.cardId}`}</Typography>
+                    {permit.forsale && <Chip label="For Sale" />}
+                    <Typography variant="subtitle1" color="text.secondary">
+                      {`Buy price : ${permit.price}`}
+                    </Typography>
+                    <Typography variant="caption">
+                      {`Discount : ${permit.discount}`}
+                    </Typography>
+                    <Typography variant="caption">
+                      {`Token ID : ${permit.tokenId}`}
+                    </Typography>
+                    <Typography variant="caption">{`Issued Time : ${permit.issuedTime}`}</Typography>
+                    {!permit.forsale && (
+                      <>
+                        <Divider light sx={{ m: 2 }} />
+                        <FormControl fullWidth sx={{ m: 1 }} variant="standard">
+                          <InputLabel htmlFor="standard-adornment-amount">
+                            Amount
+                          </InputLabel>
+                          <Input
+                            id="standard-adornment-amount"
+                            disabled={permit.forsale}
+                            name={`${permit.tokenId}-input`}
+                            startAdornment={
+                              <InputAdornment position="start">
+                                WEI
+                              </InputAdornment>
                             }
-                          }}
-                          variant="outlined"
-                        >
-                          SET FOR SALE
-                        </LoadingButton>
-                      </FormControl>
-                    </>
-                  )}
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
-      )}
+                          />
+                          <LoadingButton
+                            sx={{ mt: 1 }}
+                            disabled={permit.forsale}
+                            onClick={() => {
+                              const elements = document.getElementsByName(
+                                `${permit.tokenId}-input`
+                              ) as NodeListOf<HTMLInputElement>;
+
+                              // Check if the elements exist
+                              if (elements.length > 0) {
+                                // Access the first element in the array
+                                const item = elements[0];
+                                const price = parseInt(item.value, 10);
+
+                                setForSale(permit.tokenId, price);
+                              }
+                            }}
+                            variant="outlined"
+                          >
+                            SET FOR SALE
+                          </LoadingButton>
+                        </FormControl>
+                      </>
+                    )}
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Box>
+      <Box>
+        {tickets && (
+          <Grid container spacing={1}>
+            {tickets.map((ticket, index) => (
+              <Grid key={index} item>
+                <Card sx={{ display: "flex", m: 2, maxWidth: 300 }}>
+                  <CardContent sx={{ flex: "1 0 auto" }}>
+                    <Typography variant="h5">{`Ticket ${ticket.id}`}</Typography>
+                    <Typography variant="subtitle1" color="text.secondary">
+                      {`Price : ${ticket.price}`}
+                    </Typography>
+                    {ticket.type == 1 && <Chip label="BUS" />}
+                    {ticket.type == 2 && <Chip label="SUBWAY" />}
+                    {ticket.type == 3 && <Chip label="TRAIN" />}
+                    <Typography variant="body2">{`Expire ${expirationCountDown(
+                      ticket.expiration
+                    )}`}</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Box>
     </>
   );
 }
